f6864b50a7c7ac61a8e54515604d9d85feb38870
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-21 20:05:01 +0530

    Update Tut1.cpp

--- Linked List/Tut1.cpp
+++ Linked List/Tut1.cpp
@@ -1,4 +1,3 @@
-// A simple C++ program for traversal of a linked list
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,8 +7,6 @@ public:
 	Node* next;
 };
 
-// This function prints contents of linked list
-// starting from the given node
 void printList(Node* n)
 {
 	while (n != NULL) {
@@ -18,30 +15,26 @@ void printList(Node* n)
 	}
 }
 
-// Driver code
 int main()
 {
 	Node* head = NULL;
 	Node* second = NULL;
 	Node* third = NULL;
 
-	// allocate 3 nodes in the heap
 	head = new Node();
 	second = new Node();
 	third = new Node();
 
-	head->data = 1; // assign data in first node
-	head->next = second; // Link first node with second
+	head->data = 1;
+	head->next = second;
 
-	second->data = 2; // assign data to second node
+	second->data = 2;
 	second->next = third;
 
-	third->data = 3; // assign data to third node
+	third->data = 3;
 	third->next = NULL;
 
 	printList(head);
 
 	return 0;
 }
-
-// This is code is contributed by rathbhupendra
