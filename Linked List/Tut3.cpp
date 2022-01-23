337eb25ebd240c421c086331999a4fdc6c874fcc
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-23 11:22:47 +0530

    Inserting a node in LL

--- Linked List/Tut3.cpp
+++ Linked List/Tut3.cpp
@@ -31,13 +31,47 @@ void insertAfter( Node *prev_node, int new_data){    // Time Complexity of void 
 
 // inserting a node at the end of linked list
 
-void append( Node *head_ref, int new_data){
+void append( Node **head_ref, int new_data){
     Node *new_node = new Node();
-    while(head_ref != NULL){
-        head_ref = head_ref -> next;
-    }
+    Node *last = *head_ref;
     new_node -> data = new_data;
     new_node -> next = NULL;
-    head_ref -> next = new_node;
+    if (*head_ref == NULL){
+        *head_ref = new_node;
+        return;
+    }
+    while(last->next != NULL){
+        last = last -> next;
+    }
+    last -> next = new_node;
     return;
+}
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout<<" "<<node->data;
+        node = node->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+
+    append(&head, 6);
+
+    push(&head, 7);
+
+    push(&head, 1);
+
+    append(&head, 4);
+
+    insertAfter(head->next, 8);
+
+    cout<<"Created Linked list is: ";
+    printList(head);
+
+    return 0;
 }
